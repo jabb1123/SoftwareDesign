8a83276a03b2622d4c192b8ada4b5f66547933fa
Jacob Riedel <Jacob.Riedel@students.olin.edu>
2014-02-12 17:10:44 -0500

    For Checkin

---
+++ 2.4.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 30 14:06:58 2014
+
+@author: jacob
+"""
+
+def volume_sphere():
+    r=int(raw_input('What is the Radius?\n'))    
+    volume=r**3*math.pi*4/3.0
+    return(volume)
+    
+def 
