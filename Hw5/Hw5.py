e57a0313a94670a97ac1a0c9247624518d0b663a
Jacob Riedel <Jacob.Riedel@students.olin.edu>
2014-03-03 00:20:41 -0500

    SoftDes Homework 5

---
+++ Hw5/Hw5.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 24 14:15:27 2014
+
+@author: jacob
+"""
+
+import re
+
+def load_book(text):  
+#This function loads the book from a .txt file. It also cuts off the beginning and end tag.
+    f=open(text,'r')
+    book=f.read()
+    f.close()
+    startOfBook=book.index(' ***')+4
+    book=book[startOfBook:]
+    endOfBook=book.index('End of the Project')
+    book=book[:endOfBook]
+    return book
+
+def get_words_from_book(text):
+#Creates a list of all the words the from the string.
+    return re.findall("[a-zA-Z']+",text)
+    
+def parse_through_text(textList):
+    newText=map(lambda x: str.replace(x,'the',''),textList)
+    newText=map(lambda x: str.replace(x,'The',''),newText)
+    newText=map(lambda x: str.replace(x,'for',''),newText)
+    newText=map(lambda x: str.replace(x,'with',''),newText)
+    newText=map(lambda x: str.replace(x,'from',''),newText)
+    newText=map(lambda x: str.replace(x,'because',''),newText)
+    newText=map(lambda x: str.replace(x,'about',''),newText)
+    newText=map(lambda x: str.replace(x,'through',''),newText)
+    newText=map(lambda x: str.replace(x,'after',''),newText)
+    newText=map(lambda x: str.replace(x,'over',''),newText)
+    newText=map(lambda x: str.replace(x,'between',''),newText)
+    newText=map(lambda x: str.replace(x,'out',''),newText)
+            
+    return newText
+    
+def Compare_number_of_occurances(word1,word2,textList):
+    #Takes an input for two words and a list of words.  Returns the count of the words.
+    x=0
+    y=0
+    for i in range(len(textList)):
+        if textList[i]==word1:
+            x+=1
+        elif textList[i]==word2:
+            y+=1
+    return (x,y)
+
+    '''  
+def Analyze_bible(word1,word2):
+    bible=load_book('Bible.txt')
+    wordList=parse_through_text(bible)
+    (x,y)=Compare_number_of_occurances(word1,word2,wordList)
+    if x>y:
+        print 'There were '+str(x)+' occurances of the word '+word1 +' and '+str(y)+' occurances of '+word2
+    else:
+         print 'There were '+str(y)+' occurances of the word '+word2+' and '+str(x)+' occurances of '+word1
+    '''
